ae3278754f55e2eb08b49df4774f81e34dbbe320
Rebeca Aliseda <rebecaaliseda26@gmail.com>
2024-04-14 13:13:59 +0200

    Laboratorio Modulo 3

---
+++ src/main.ts
@@ -0,0 +1,55 @@
+import './style.css';
+
+var grupoMusical = '%cThe Beatles';
+var año = '1960';
+var activo = true;
+var generoMusical = '🎵Pop Rock';
+const estiloTitulo = 'font-weight:bold; background-color:#00FF00; font-size:20px';
+console.log(grupoMusical, estiloTitulo);
+console.log('Año fundación: ' + año);
+console.log('Activo: ' + activo);
+console.log('Género musical: ' + generoMusical);
+
+console.log('----------------------');
+
+var grupoMusical = '%cQueen';
+var año = '1970';
+var activo = false;
+var generoMusical = '🎸Rock';
+console.log(grupoMusical, estiloTitulo);
+console.log('Año fundación: ' + año);
+console.log('Activo: ' + activo);
+console.log('Género musical: ' + generoMusical);
+
+console.log('----------------------');
+
+var grupoMusical = '%cAC DC';
+var año = '1973';
+var activo = true;
+var generoMusical = '🤘Hard Rock';
+console.log(grupoMusical, estiloTitulo);
+console.log('Año fundación: ' + año);
+console.log('Activo: ' + activo);
+console.log('Género musical: ' + generoMusical);
+
+console.log('----------------------');
+
+var grupoMusical = '%cLudwig van Beethoven';
+var año = '1770';
+var activo = false;
+var generoMusical = '🎼Clásica';
+console.log(grupoMusical, estiloTitulo);
+console.log('Año fundación: ' + año);
+console.log('Activo: ' + activo);
+console.log('Género musical: ' + generoMusical);
+
+console.log('----------------------');
+
+var grupoMusical = '%cThe Rolling Stones';
+var año = '1962';
+var activo = true;
+var generoMusical = '🎸Rock';
+console.log(grupoMusical, estiloTitulo);
+console.log('Año fundación: ' + año);
+console.log('Activo: ' + activo);
+console.log('Género musical: ' + generoMusical);
