6c56a27eb3624e8ac67f9403db5bde421af42688
Rebeca Aliseda <rebecaaliseda26@gmail.com>
2024-04-14 13:49:19 +0200

    Laboratorio Modulo 3 utilizando una interfaz

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,77 @@
 import './style.css';
 
-var grupoMusical = '%cThe Beatles';
-var año = '1960';
-var activo = true;
-var generoMusical = '🎵Pop Rock';
+// Creamos la interfaz
+interface GrupoMusical {
+  nombre: string;
+  año: number;
+  activo: boolean;
+  generoMusical: string;
+}
+
+// Definimos los distintos grupos
+const grupo1: GrupoMusical = {
+  nombre: '%cThe Beatles',
+  año: 1960,
+  activo: true,
+  generoMusical: '🎵Pop Rock',
+};
+const grupo2: GrupoMusical = {
+  nombre: '%cQueen',
+  año: 1970,
+  activo: false,
+  generoMusical: '🎸Rock',
+};
+const grupo3: GrupoMusical = {
+  nombre: '%cAC DC',
+  año: 1973,
+  activo: true,
+  generoMusical: '🤘Hard Rock',
+};
+const grupo4: GrupoMusical = {
+  nombre: '%cLudwig van Beethoven',
+  año: 1770,
+  activo: false,
+  generoMusical: '🎼Clásica',
+};
+const grupo5: GrupoMusical = {
+  nombre: '%cThe Rolling Stones',
+  año: 1962,
+  activo: true,
+  generoMusical: '🎸Rock',
+};
+// Definimos estilo del título
 const estiloTitulo = 'font-weight:bold; background-color:#00FF00; font-size:20px';
-console.log(grupoMusical, estiloTitulo);
-console.log('Año fundación: ' + año);
-console.log('Activo: ' + activo);
-console.log('Género musical: ' + generoMusical);
 
-console.log('----------------------');
+//Mostramos por consola todos los grupos
+console.log(grupo1.nombre, estiloTitulo);
+console.log('Año fundación: ' + grupo1.año);
+console.log('Activo: ' + grupo1.activo);
+console.log('Género musical: ' + grupo1.generoMusical);
 
-grupoMusical = '%cQueen';
-año = '1970';
-activo = false;
-generoMusical = '🎸Rock';
-console.log(grupoMusical, estiloTitulo);
-console.log('Año fundación: ' + año);
-console.log('Activo: ' + activo);
-console.log('Género musical: ' + generoMusical);
+console.log('--------------------------');
 
-console.log('----------------------');
+console.log(grupo2.nombre, estiloTitulo);
+console.log('Año fundación: ' + grupo2.año);
+console.log('Activo: ' + grupo2.activo);
+console.log('Género musical: ' + grupo2.generoMusical);
 
-grupoMusical = '%cAC DC';
-año = '1973';
-activo = true;
-generoMusical = '🤘Hard Rock';
-console.log(grupoMusical, estiloTitulo);
-console.log('Año fundación: ' + año);
-console.log('Activo: ' + activo);
-console.log('Género musical: ' + generoMusical);
+console.log('--------------------------');
 
-console.log('----------------------');
+console.log(grupo3.nombre, estiloTitulo);
+console.log('Año fundación: ' + grupo3.año);
+console.log('Activo: ' + grupo3.activo);
+console.log('Género musical: ' + grupo3.generoMusical);
 
-grupoMusical = '%cLudwig van Beethoven';
-año = '1770';
-activo = false;
-generoMusical = '🎼Clásica';
-console.log(grupoMusical, estiloTitulo);
-console.log('Año fundación: ' + año);
-console.log('Activo: ' + activo);
-console.log('Género musical: ' + generoMusical);
+console.log('--------------------------');
 
-console.log('----------------------');
+console.log(grupo4.nombre, estiloTitulo);
+console.log('Año fundación: ' + grupo4.año);
+console.log('Activo: ' + grupo4.activo);
+console.log('Género musical: ' + grupo4.generoMusical);
 
-grupoMusical = '%cThe Rolling Stones';
-año = '1962';
-activo = true;
-generoMusical = '🎸Rock';
-console.log(grupoMusical, estiloTitulo);
-console.log('Año fundación: ' + año);
-console.log('Activo: ' + activo);
-console.log('Género musical: ' + generoMusical);
+console.log('--------------------------');
+
+console.log(grupo5.nombre, estiloTitulo);
+console.log('Año fundación: ' + grupo5.año);
+console.log('Activo: ' + grupo5.activo);
+console.log('Género musical: ' + grupo5.generoMusical);
